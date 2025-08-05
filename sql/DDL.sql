3a3a5b2b21bda4bf32636be728ba9d7a994d3c26
Seonguk-j <deas124@gmail.com>
2025-08-05 21:43:20 +0900

    feat: BaseEntity 추가

--- sql/DDL.sql
+++
@@ -1,9 +0,0 @@
-// schema.sql
-create table if not exists person
-(
-    person_id   bigint  primary key auto_increment  not null,
-    name        varchar(20),
-    job         varchar(20),
-    age         int,
-    hobby       varchar(20)
-)
