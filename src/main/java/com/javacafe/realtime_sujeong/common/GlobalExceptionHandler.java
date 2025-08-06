9ad77baa7144eed2e613eb9c1425071384409240
Seonguk-j <deas124@gmail.com>
2025-08-06 20:33:30 +0900

    Refactor: BaseEntity, GlobalExceptionHandler 수정

--- src/main/java/com/javacafe/realtime_sujeong/common/GlobalExceptionHandler.java
+++ src/main/java/com/javacafe/realtime_sujeong/common/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.javacafe.realtime_sujeong.common;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,6 +11,20 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	// @Valid에서 발생하는 예외 처리
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+
+		ex.getBindingResult().getFieldErrors().forEach(error ->
+				errors.put(error.getField(), error.getDefaultMessage())
+		);
+
+		return ResponseEntity
+				.status(HttpStatus.BAD_REQUEST)
+				.body(errors);
+	}
 
 	// @Validated (단일 파라미터 유효성)에서 발생하는 예외 처리
 	@ExceptionHandler(ConstraintViolationException.class)
